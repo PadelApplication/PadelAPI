5cb96086aefb8bd7688e21475801b0252cb241a3
SteijnPloegmakers <sploegmakers@inforit.nl>
2023-10-02 17:09:56 +0200

    Add project files.

---
+++ PadelAPI/Models/Padel.cs
@@ -0,0 +1,13 @@
+﻿namespace PadelAPI.Models
+{
+    public class Padel
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public string? Place { get; set; }
+        public string? Court { get; set; }
+        public int People { get; set; }
+    }
+}
